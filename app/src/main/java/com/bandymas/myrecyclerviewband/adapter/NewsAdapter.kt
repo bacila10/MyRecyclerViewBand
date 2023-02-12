4576cfd8af7e0065c9b4764e7f4f9b727dbed5c8
bacila10 <Vaidas10>
2023-02-12 22:03:44 +0200

    darasysti komentarai adapteryje

--- app/src/main/java/com/bandymas/myrecyclerviewband/adapter/NewsAdapter.kt
+++ app/src/main/java/com/bandymas/myrecyclerviewband/adapter/NewsAdapter.kt
@@ -16,20 +16,28 @@ class NewsAdapter(private val dummyList: List<DummyData>): RecyclerView.Adapter<
   //  private lateinit var binding: ItemsDummyDataBinding
   //val largeDummyList = AddItems.getDummyData(this)
 
-
+//Jis sukuria "View" su kuriais dirbsime (kiek matosi ekrane + keletas virsuje ir apacioje)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsHolder {
        val binding = LayoutInflater.from(parent.context).inflate(R.layout.items_dummy_data, parent, false)
         return NewsHolder(binding)
     }
 
+
+//Is saraso gauname elementa, pozicija ir ikeliame atitinkamus reiksmes
+//Iskvieciamas kiekvienam saraso elementui
     override fun onBindViewHolder(holder: NewsHolder, position: Int) {
         val item = dummyList[position]
         holder.itemTextView.text = item.title
         holder.itemImageView.setImageDrawable(item.image)
+        holder.itemView.setOnLongClickListener(){
+true
+        }
     }
 
     override fun getItemCount(): Int = dummyList.size
 
+
+//Saugo "onCreateViewHolder" sukurtus "View". Juose gauname elementus kur galimaivesti tekstus
     class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var itemTextView = itemView.findViewById<TextView>(R.id.txtDummy)
         var itemImageView = itemView.findViewById<ImageView>(R.id.imgDummy)
